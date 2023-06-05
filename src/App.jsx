9fe1493ac33f502a86a7beae184a216169d4cf25
Michael Kim <michael.dev.kim@gmail.com>
2023-06-04 17:02:56 -0700

    created left component -getting data

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,13 @@
 /* eslint-disable no-unused-vars */
-import { useState } from 'react';
 import './App.css';
+import Left from './Left';
 
-function App() {
-  const [data, setData] = useState();
-  const [sData, setSData] = useState([]);
 
-  const invokeURL = "https://feuzl6d9yk.execute-api.us-west-1.amazonaws.com/test";
-
-  async function test() {
-    let res = await fetch(invokeURL + "/health");
-    let jsonData = await res.json();
-    console.log("********* jsonData **********");
-    console.log(jsonData);
-
-    setData(jsonData);
-
-    console.log("********* data **********");
-    console.log(data);
-  }
-
-  async function getStudents() {
-    let res = await fetch(invokeURL + "/students");
-    let jsonData = await res.json();
-    console.log("********* jsonData **********");
-    console.log(jsonData);
-
-    setSData(jsonData);
-
-    console.log("********* data **********");
-    console.log(sData);
-  }
-
+export default function App() {
   return (
     <>
-      <h1>App.jsx</h1>
-      <button onClick={test}>Test</button>
-      <br />
-      {data}
-      <br />
-      <button onClick={getStudents}>Students</button>
-      <br />
-      {sData.length > 1 ? (
-          sData.map((student) => {
-            return(
-              <>
-                <h3>{student.first_name}</h3>
-                <h3>{student.last_name}</h3>
-                <h3>{student.email}</h3>
-              </>
-            )
-        })
-      ) : (
-        <p>No student data available.</p>
-      )
-      }
+    App.jsx
+    <Left />
     </>
   );
 }
-
-export default App;
---
+++ src/Left.jsx
@@ -0,0 +1,52 @@
+/* eslint-disable no-unused-vars */
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+export default function Left() {
+  const invokeURL = "https://feuzl6d9yk.execute-api.us-west-1.amazonaws.com/test";
+
+  const [selectedDatabase, setSelectedDatabase] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState(null);
+
+  const handleDatabaseChange = (event) => {
+    setSelectedDatabase(event.target.value);
+  };
+
+  const handleLoadClick = async () => {
+    if (!selectedDatabase) {
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const response = await axios.get(invokeURL + `/${selectedDatabase}`);
+      setData(response.data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+
+  return (
+    <div>
+      <h1> Left.jsx</h1>
+      <select value={selectedDatabase} onChange={handleDatabaseChange}>
+        <option value="">Select a database</option>
+        <option value="students">Students</option>
+      </select>
+      <button onClick={handleLoadClick} disabled={!selectedDatabase || loading}>
+        {loading ? 'Loading...' : 'Load'}
+      </button>
+      {data && (
+        <div>
+          <h2>Data:</h2>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+  );
+}
